9d358f08c9e4a0998b28a812cf33827e89fc3ae6
joseLopez <kota012011@gmail.com>
2025-03-26 16:51:34 -0500

    ESTILOS CSS

--- resources/views/departamento/index.blade.php
+++ resources/views/departamento/index.blade.php
@@ -4,14 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/appGeneral.css') }}" rel="stylesheet">
     <title>Departamentos</title>
   </head>
   <body>
     <nav class="navbar bg-cyan-950">
         <form class="container-fluid justify-content-start">
-            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/comuna'">IR a Comunas</button>
-            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/municipio'">IR a Municipios</button>
-            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/pais'">IR a Paises</button>
+            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/comuna'">IR a Comunas 🫂</button>
+            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/municipio'">IR a Municipios 🔜</button>
+            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/pais'">IR a Paises 🏴</button>
         </form>
     </nav>
     <div class="container">
@@ -33,12 +34,13 @@
                     <td>{{ $departamento->depa_nomb }}</td>
                     <td>{{ $departamento->pais_nomb }}</td>
                     <td>
-                        <a href="{{ route('departamento.edit', ['departamento' => $departamento->depa_codi]) }}" class="btn btn-info">Edit</a>
+                        
                         <form action="{{ route('departamento.destroy', ['departamento' => $departamento->depa_codi]) }}" method="POST" style="display:inline-block" onsubmit="return confirm('¿Seguro que quieres eliminar este departamento?')">
                             @method('DELETE')
                             @csrf
-                            <button type="submit" class="btn btn-danger">Delete</button>
+                            <a href="{{ route('departamento.edit', ['departamento' => $departamento->depa_codi]) }}" class="btn btn-info">Edit</a>
                         </form>
+                        <button type="submit" class="btn btn-danger">Delete</button>
                     </td>
                 </tr>
                 @endforeach
--- resources/views/pais/index.blade.php
+++ resources/views/pais/index.blade.php
@@ -4,14 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{ asset('css/appGeneral.css') }}" rel="stylesheet">
     <title>Países</title>
   </head>
   <body>
     <nav class="navbar bg-cyan-950">
         <form class="container-fluid justify-content-start">
-            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/comuna'">IR a Comunas</button>
-            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/municipio'">IR a Municipios</button>
-            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/departamento'">IR a Departamentos</button>
+            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/comuna'">IR a Comunas 🕵🏻‍♂️</button>
+            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/municipio'">IR a Municipios 🔜</button>
+            <button class="btn btn-outline-success me-2" type="button" onclick="window.location.href='http://127.0.0.1:8000/departamento'">IR a Departamentos 🫂</button>
         </form>
     </nav>
     <div class="container">
